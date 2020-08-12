dbc6fbbfa151e6e3858b2d76d4e548010b2b201c
Rajat Gundi <glrajat@gmail.com>
2020-08-12 16:36:21 +0530

    Reading Input Variables a,b & c

--- arithmaticOperations_Stage2.sh
+++ arithmaticOperations_Stage2.sh
@@ -2,3 +2,10 @@
 
 #Master's content
 echo "Some Basic Arithmatic Operations Stage2"
+
+#ReadingInputVariables
+echo "Enter the follow values (Intergers Only)*"
+read -p "a :" a
+read -p "b :" b
+read -p "c :" c
+
