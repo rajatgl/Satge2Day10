d2c3a1c7d472dd52edc0c5610ad1bcd333a9e6b0
Rajat Gundi <glrajat@gmail.com>
2020-08-13 19:08:13 +0530

    Simulation Begins

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -4,4 +4,16 @@
 #MasterMessage
 echo "Welcome To Flip-Coin Simulator"
 
-
+#SimulationBegins
+coinCheck=$((RANDOM%2))
+echo "The Outcome Is :"
+flipCoinSimulator()
+{
+	if [ $1 -eq 1 ]
+	then
+		echo "Heads"
+	else
+		echo "Tails"
+	fi
+}
+flipCoinSimulator $coinCheck
