c0a66722b5c109da1ba267b2662b6ebac59e7e87
Rajat Gundi <glrajat@gmail.com>
2020-08-13 19:50:28 +0530

    Looping On Simulator & counting each outcome

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -2,18 +2,37 @@
 
 
 #MasterMessage
-echo "Welcome To Flip-Coin Simulator"
+echo $'Welcome To Flip-Coin Simulator\n'
+
+#constants
+Heads=0
+Tails=0
 
 #SimulationBegins
-coinCheck=$((RANDOM%2))
-echo "The Outcome Is :"
+#coinCheck=$((RANDOM%2))
+read -p "Enter Number Of Flips :" n
+echo $'The Outcome Is : \n'
+
+#SimulatorFunction
 flipCoinSimulator()
 {
 	if [ $1 -eq 1 ]
 	then
-		echo "Heads"
+		echo "H"
+		((Heads++))
 	else
-		echo "Tails"
+		echo "T"
+		((Tails++))
 	fi
 }
-flipCoinSimulator $coinCheck
+#flipCoinSimulator $coinCheck
+
+#Looping On Coin Simulator and cointing no. of wins for each outcome
+for (( i=0 ; i<$n ; i++))
+do
+	coinCheck=$((RANDOM%2))
+	flipCoinSimulator $coinCheck
+done
+echo ""
+echo "Number Of Heads : $Heads"
+echo "Number Of Tails : $Tails"
